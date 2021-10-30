7ce7740db3d4a287e31b36987f285b86090b4819
Sincherz <81931926+Sincherz@users.noreply.github.com>
2021-10-30 01:34:04 -0400

    Add files via upload

---
+++ Maps Microservice/app.py
@@ -0,0 +1,23 @@
+from flask_cors import CORS
+from flask import Flask, jsonify
+
+from processdata import processdata
+
+app = Flask(__name)
+CORS(app)
+
+# In this case, the URL route is 'displaylocations'.
+@app.route('/displaylocation')
+
+def displaylocations():
+
+    # Import data from CSV file
+    data = processdata()
+
+    # Forward the data to the source that called this API.
+    return jsonify(data)
+
+    if __name__ == '__main__':
+        app.run(host = <host_name>, debug = True, port = <port_no>)
+
+    python app.py
