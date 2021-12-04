065b7cd1ddb038fbcea538accce39f46b103f1f8
Sincherz <81931926+Sincherz@users.noreply.github.com>
2021-12-03 22:40:23 -0500

    Updates to js files
    loaddata - to pull info from peer's microservice and display to page
    maps - changed parameters

--- public/js/loaddata.js
+++ public/js/loaddata.js
@@ -1,7 +1,32 @@
-fetch('http://flip3.engr.oregonstate.edu:33233/ChIJW69I7FhZwokR61IbDPnsqTo')
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.dir(myJson);
-  });
+      let holddata;
+      let element;
+      element = document.getElementById("form");
+
+      fetch(
+        "http://flip3.engr.oregonstate.edu:33233/ChIJW69I7FhZwokR61IbDPnsqTo"
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+
+          for (let i = 0; i< 3; i++){
+            
+            holddata = document.createElement("p");
+            //holddata.appendChild(document.createTextNode("Rating: "));
+            //holddata.appendChild(document.createTextNode("Rating: "));
+            //console.log(holddata.outerHTML);
+            holddata.innerText = data[i].rating;
+            document.getElementById("form").appendChild(holddata) ;
+
+            holddata = document.createElement("p");
+            holddata.innerText = data[i].text;
+            document.getElementById("form").appendChild(holddata);
+            
+          }
+          
+        })
+        .catch(function () {
+          console.log("Booo");
+        });
--- public/js/maps.js
+++ public/js/maps.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3000;
 app.get("/:mysearch", function (req, res) {
   // We create an html file:
 
-  var s = "https://maps.googleapis.com/maps/api/staticmap?center=";
+  var url = "https://maps.googleapis.com/maps/api/staticmap?center=";
   var add = req.params.mysearch;
-  var p =
-    "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyC9jRdbgKD9BOaVEOe_eHNsmdDlYhO5rac";
-  s = s + add + p;
-  console.log(s);
-  res.send(s);
+  var parameter =
+    "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:url%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyDcJ2w-Ccyga1ok-p59mdRLt8UVK1u2arE";
+  url = url + add + parameter;
+  console.log(url);
+  res.send(url);
 });
 
 const hostname = "localhost";
